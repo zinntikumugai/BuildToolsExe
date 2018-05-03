a350d458738476ca9af785656c8fb112321a19b5
zinntikumugai <uesitananame55@gmail.com>
2018-05-03 13:53:45 +0900

    fast commit

---
+++ buildTools.bat
@@ -0,0 +1,13 @@
+@echo off
+cd /d %~dp0
+set buildtoolsurl=https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+set buildtoolsfile=%~dp0%BuildTools.jar
+
+bitsadmin /TRANSFER dl "%buildtoolsurl%" "%buildtoolsfile%"
+if EXIST "%buildtoolsfile%" (
+    call java -jar "%buildtoolsfile%"
+) ELSE (
+    echo "Download Faild."
+)
+
+pause
