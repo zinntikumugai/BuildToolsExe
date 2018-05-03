b2a89930a864f97bc63f43c09673a0a5b842e5c2
zinntikumugai <uesitananame55@gmail.com>
2018-05-03 13:57:48 +0900

    fix/bat フォルダ内でビルドするように変更

--- buildTools.bat
+++ buildTools.bat
@@ -1,7 +1,11 @@
 @echo off
 cd /d %~dp0
 set buildtoolsurl=https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-set buildtoolsfile=%~dp0%BuildTools.jar
+set buildtoolsfile=%~dp0%spigot\\BuildTools.jar
+
+if NOT EXIST "spigot" (
+    mkdir spigot
+)
 
 bitsadmin /TRANSFER dl "%buildtoolsurl%" "%buildtoolsfile%"
 if EXIST "%buildtoolsfile%" (
